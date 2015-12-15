1caf7846b859c5ce04c322ead84e9c1748193f61
Dext3rLab <venturachristopher98@gmail.com>
2015-12-15 17:25:45 -0400

    Update Perceptron_Simple.c

--- Perceptron_Simple.c
+++ Perceptron_Simple.c
@@ -6,16 +6,15 @@
 void Perceptron(){
 	srand(time(NULL));
 	float intervalos[] = {-0.98,-0.876,-0.654,-0.754,0.453,0.953,-0.322,-0.298,-0.11,-0.065,0.0234,0.232,0.2335,0.4674,0.675,0.7883, 0.752,0.823,0.9876};
-	float w1_peso,w2_peso,Umbral,defumbral,factor;
+	float Umbral,defumbral,factor;
 	int i;
 	int x1[] = {1,1,-1,-1};
 	int x2[] = {1,-1,1,-1};
 	int result[] = {1,-1,-1,-1};
-	w1_peso = intervalos[rand() % 18];
-	w2_peso = intervalos[rand() % 18];
+	float w[2] = {intervalos[rand() % 18],intervalos[rand() % 18]};
 	factor = intervalos[rand() % 18];
 	Umbral = intervalos[rand() % 18];
-	defumbral = -1;
+	defumbral = 1;
 	int verdad = 0;
 	int op;
 	int cont = 0;
@@ -23,25 +22,22 @@ void Perceptron(){
 //////////// ENTRENAMIENTO DEL PERCEPTRON;
 	while(verdad == 0){
 		n++;
-		//printf("--------------------- Epoca %i ----------------------\n\n",n);
 		for(i=0;i<4;i++){
-			//printf("[*] Pesos Sinapticos: %.2f & %.2f -- Umbral: %f\n",w1_peso,w2_peso,Umbral);
-			op = ((x1[i]*w1_peso)+(x2[i]*w2_peso)) + (defumbral*Umbral);
+			op = ((x1[i]*w[0])+(x2[i]*w[1])) + (defumbral*Umbral);
 			if(op >= 0){
 				op = 1;
 			}
 			else{
 				op = -1;
 			}
-			//printf("\tSalida: %2i --> Patron: %2i\n\n",op,result[i]);
 			if(op != result[i]){
-				w1_peso = w1_peso + (2*factor)*(x1[i]*result[i]);
-				w2_peso = w2_peso + (2*factor)*(x2[i]*result[i]);
+				w[0] = w[0] + (2*factor)*(x1[i]*result[i]);
+				w[1] = w[1] + (2*factor)*(x2[i]*result[i]);
 				Umbral = Umbral + (2*factor)*(defumbral*result[i]);
 			}
 		}
 		for(i=0;i<4;i++){
-			op = ((x1[i]*w1_peso)+(x2[i]*w2_peso)) + (defumbral*Umbral);
+			op = ((x1[i]*w[0])+(x2[i]*w[1])) + (defumbral*Umbral);
 			if(op == result[i]){
 				cont++;
 			}
@@ -61,12 +57,12 @@ void Perceptron(){
 	printf("[*] PERCEPTRON SIMPLE [ENTRENADO]\n");
 	printf("\n--------------- DATOS FINALES --------------------------------\n\n");
 	printf("[*] Total de epocas: (%i)\n",n);
-	printf("[*] Peso Sinaptico 1\t\t--> %2.2f\n",w1_peso);
-	printf("[*] Peso Sinaptico 2\t\t--> %2.2f\n",w2_peso);
-	printf("[*] Umbral(Polarizacion)\t--> %2.2f\n",Umbral);
+	printf("[*] Peso Sinaptico 1 w[1]\t\t--> %2.2f\n",w[0]);
+	printf("[*] Peso Sinaptico 2 w[2]\t\t--> %2.2f\n",w[1]);
+	printf("[*] Umbral(Polarizacion)\t\t--> %2.2f\n",Umbral);
 	printf("\n\n--------------- VERFICIACION DE ENTRENAMIENTO ------------------\n\n");
 	for(i=0;i<4;i++){
-		op = ((x1[i]*w1_peso)+(x2[i]*w2_peso)) + (defumbral*Umbral);
+		op = ((x1[i]*w[0])+(x2[i]*w[1])) + (defumbral*Umbral);
 		if(op >= 0){
 			op = 1;
 		}
