40d0fb77352683da7667ddbb3df003f53e7d1749
Dext3rLab <venturachristopher98@gmail.com>
2015-12-29 13:01:38 -0400

    Create main.cpp

---
+++ Agenda/main.cpp
@@ -0,0 +1,8 @@
+#include "contacto.h"
+
+int main(int args, char **argv){
+	contacto[0].Asignar("Sin Asignar","Sin Asignar","Sin Asignar"); // contacto de Ejemplo
+	agenda.Guardar(); // guardamos la agenda
+	Programa programa;
+	return 0;
+}
